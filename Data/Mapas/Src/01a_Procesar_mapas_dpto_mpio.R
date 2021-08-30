319858aef2d8bf475a4a2d6778fb2dfd50864067
XPS-ANA\Ana <ana.pirela5@gmail.com>
2021-08-30 08:49:14 -0500

    Mapas

---
+++ Data/Mapas/Src/01a_Procesar_mapas_dpto_mpio.R
@@ -0,0 +1,81 @@
+#-------------------------------------------------------#
+# Pulso Social BID ----
+# Ultima fecha de modificacion: 30 agosto, 2021
+# Procesar mapas departamentales y municipales para creacion de graficas
+#-------------------------------------------------------#
+
+#--------------------------#
+# packages ----
+#--------------------------#
+
+rm(list=ls())
+pacman::p_load(tidyverse, glue, sf, raster)
+# .rs.restartR()
+
+#--------------------------#
+# paths ----
+#--------------------------#
+
+datos_ori <- "Data/Mapas/Input"
+datos <- "Data/Mapas/Output"
+options(scipen = 999)
+
+#-------------------------------------------------------#
+# 1. Abrir shapefiles ----
+#-------------------------------------------------------#
+
+#--------------------------#
+# A. Departamentos ----
+#--------------------------#
+
+# Mapas a nivel departamental
+map_dptos <- st_read("Data/Mapas/Input/MGN_DPTO_POLITICO.shp") %>% 
+  dplyr::select(DPTO_CCDGO) %>% rename(cod_dpto = DPTO_CCDGO) %>% mutate(cod_dpto = as.numeric(cod_dpto))
+
+# Transformamos a CRS Magna Sirgas (proyectado) para simplificar correctamente
+projcrs <- "+proj=tmerc +lat_0=4.596200416666666 +lon_0=-74.07750791666666 +k=1 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+map_dptos <- sf::st_transform(map_dptos, crs = CRS('+init=EPSG:3116'))
+map_dptos <- st_set_crs(map_dptos, projcrs)
+
+# Simplificamos la geometria de los poligonos: evita bordes muy detallados que consumen memoria
+map_dptos <- st_simplify(map_dptos, preserveTopology = FALSE, dTolerance = 1000)
+
+# Transformamos a CRS 4326
+lonlat <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+map_dptos <- st_transform(map_dptos, crs = CRS('+init=EPSG:4326'))
+map_dptos <- st_set_crs(map_dptos, lonlat)
+
+# Extraemos coordenadas (para etiquetas de departamento)
+map_dptos <- cbind(map_dptos, st_coordinates(st_centroid(map_dptos)))
+head(map_dptos)
+
+# Guardamos mapa
+st_write(map_dptos, glue("{datos}/mapa_departamentos_colombia.shp"))
+
+#--------------------------#
+# B. Municipios ----
+#--------------------------#
+
+# Mapas a nivel municipal
+map_mpios <- st_read("Data/Mapas/Input/MGN_MPIO_POLITICO.shp") %>% 
+  dplyr::select(DPTO_CCDGO, DPTO_CNMBR, MPIO_CCDGO, MPIO_CNMBR) %>% 
+  rename(cod_dpto = DPTO_CCDGO, nom_dpto = DPTO_CNMBR, cod_mpio = MPIO_CCDGO, nom_mpio = MPIO_CNMBR) %>% 
+  mutate(cod_mpio = as.numeric(glue("{cod_dpto}{cod_mpio}")), cod_dpto = as.numeric(cod_dpto),
+         nom_dpto = str_to_title(nom_dpto), nom_mpio = str_to_title(nom_mpio))
+
+# Transformamos a CRS Magna Sirgas (proyectado) para simplificar correctamente
+map_mpios <- sf::st_transform(map_mpios, crs = CRS('+init=EPSG:3116'))
+map_mpios <- st_set_crs(map_mpios, projcrs)
+
+# Simplificamos la geometria de los poligonos
+map_mpios <- st_simplify(map_mpios, preserveTopology = FALSE, dTolerance = 1000)
+
+# Transformamos a CRS 4326 (Original: 4686 Magna Sirgas 2020)
+map_mpios <- st_transform(map_mpios, crs = CRS('+init=EPSG:4326'))
+map_mpios <- st_set_crs(map_mpios, lonlat)
+
+# Extraemos coordenadas (para etiquetas de municipio)
+map_mpios <- cbind(map_mpios, st_coordinates(st_centroid(map_mpios)))
+
+# Guardamos mapa
+st_write(map_mpios, glue("{datos}/mapa_municipios_colombia.shp"))
--- Descriptives/Herramientas/Src/01a_funciones_graficas.R
+++ Descriptives/Herramientas/Src/01a_funciones_graficas.R
@@ -1,6 +1,6 @@
 #-------------------------------------------------------#
 # Pulso Social BID ----
-# Ultima fecha de modificacion: 27 agosto, 2021
+# Ultima fecha de modificacion: 30 agosto, 2021
 # Funciones para creacion de graficas
 #-------------------------------------------------------#
 
@@ -9,7 +9,7 @@
 #--------------------------#
 
 rm(list=ls())
-pacman::p_load(tidyverse, glue)
+pacman::p_load(tidyverse, glue, sf)
 # .rs.restartR()
 
 #--------------------------#
@@ -21,105 +21,82 @@ datos <- "Descriptives/Herramientas/Output"
 options(scipen = 999)
 
 #-------------------------------------------------------#
-# 0. Abrir datos ----
+# 0. Funciones ----
 #-------------------------------------------------------#
 
-# Etiquetas nombres departamentos
-nom_dpto <- readxl::read_xlsx(glue("{datos_ori}/base_nombres_departamentos.xlsx")) 
+#--------------------------#
+# A. fun_data ----
+# Organiza los datos para construccion de graficas
+#--------------------------#
 
-# Abrimos datos de ejemplo
-geih <- readxl::read_xlsx("Data/Ejemplo_base_datos/Output/geih_datos_ejemplo.xlsx", sheet = "departamental") %>%
-  mutate(value_label = "Tasa de desempleo (%)")
-
-geih_gen <- readxl::read_xlsx("Data/Ejemplo_base_datos/Output/geih_datos_ejemplo.xlsx", sheet = "departamental_genero") %>%
-  mutate(value_label = "Tasa de desempleo (%)")
-
-# data <- geih
-data <- geih_gen
-
-# Emparejamos base de datos con codigos departamento
-
-if(unique(data$id_nivel) == "dpto"){
-  data <- data %>% left_join(nom_dpto, by = "nivel_value")
-} 
-
-if(grepl("gen", unique(data$id_nivel))){
-  data <- data %>%
-    dplyr::rename(nivel_value_ori = nivel_value) %>%
-    mutate(nivel_value = as.numeric(gsub("\\_.*", "", nivel_value_ori)),
-           genero = gsub(".*\\_", "", nivel_value_ori),
-           genero = ifelse(genero == 1, "Mujer", "Hombre")) %>% 
-    left_join(nom_dpto, by = "nivel_value")
+fun_data <- function(df){
+  
+  # Etiquetas nombres departamentos
+  nom_dpto <- readxl::read_xlsx(glue("{datos_ori}/base_nombres_departamentos.xlsx")) 
+  
+  # Identificar si existen desagregaciones en los datos (genero, jovenes, minorias)
+  # Sin desagregacion
+  if(unique(df$id_nivel) == "dpto"){
+    data <- df %>% left_join(nom_dpto, by = "nivel_value")
+  } 
+  
+  # Desagregacion de genero
+  if(grepl("gen", unique(df$id_nivel))){
+    data <- df %>%
+      dplyr::rename(nivel_value_ori = nivel_value) %>%
+      mutate(nivel_value = as.numeric(gsub("\\_.*", "", nivel_value_ori)),
+             genero = gsub(".*\\_", "", nivel_value_ori),
+             genero = ifelse(genero == 1, "Mujer", "Hombre")) %>% 
+      left_join(nom_dpto, by = "nivel_value")
+  }
+  
+  return(data)
 }
 
-#-------------------------------------------------------#
-# 1. Funciones ----
-#-------------------------------------------------------#
-
 #--------------------------#
-# A. Opciones generales ----
-#--------------------------#
-
-# Tamanos graficas (ancho, alto, texto, resolucion)
-w <- 4.5*2.5
-h <- 3.2*2.5
-text <- 14
-d <- 900
-s <- 1.2
-a_line <- 0.6
-a_dot <- 0.7
-
-# Tema
-theme <- list(
-  theme_classic(base_size = text*1.5),
-  theme(axis.title.x = element_text(colour = "black"),
-        axis.title.y = element_text(colour = "black"),
-        axis.text.x = element_text(colour = "black"),
-        axis.text.y = element_text(colour = "black"),
-        legend.position = "bottom",
-        plot.title = element_text(hjust = 0.5),
-        legend.title = element_blank())
-)
-
-# Crear etiquetas ejes X y Y 
-xlab_time <- ifelse(unique(data$id_time) == 1, "Año",
-                    ifelse(unique(data$id_time == 2, "Mes", 
-                                  ifelse(unique(data$time) == 3, "Día", NULL))))
-xlab_terr <- ifelse(grepl("dpto", unique(data$id_nivel)), "Departamento",
-                    ifelse(grepl("mpio", unique(data$id_nivel)), "Municipio", NULL))
-ylab <- unique(data$value_label)
-
-# Etiquetas de grafica de promedios temporales (anios, meses, dias)
-options_time <- list(
-  xlab(glue("\n{xlab_time}")),
-  ylab(glue("{ylab}\n"))
-)
-
-# Etiquetas de grafica de promedios territoriales (departamentos, municipios)
-options_terr <- list(
-  xlab(glue("\n{xlab_terr}")), 
-  ylab(glue("{ylab}\n"))
-)
-
-#--------------------------#
-# B. Colores ----
-# Fuentes: 
-# 1. https://publications.iadb.org/publications/spanish/document/Pulso-social-en-Am%C3%A9rica-Latina-y-el-Caribe-2017-Legado-familiar-%C2%BFrompemos-el-molde-o-repetimos-patrones.pdf
-# 2. https://publications.iadb.org/publications/spanish/document/Pulso-social-de-Am%C3%A9rica-Latina-y-el-Caribe-2016-Realidades-y-perspectivas.pdf
-#--------------------------#
-
-bar_colors1 <- c("#3d5574", "#74ac54", "#b8544c", "#c0ac84", "#c8dccc", "#b0c4e4", "#d4742c")
-lines_colors1 <- c("#3d5574", "#74ac54")
-countries_colors1 <- c("#759cd4", "#c43424", "#ecbc24", "#385676", "#d4742d","#6dad55", "#a4a4a4", "#643d94")
-
-bar_colors2 <- c("#4c9ccc", "#e4546c", "#fcac24", "#0c0c0c", "#b4b4b4")
-lines_colors2 <- c("#4c9ccc", "#e4546c", "#fcac24", "#0c0c0c")
-
-#--------------------------#
-# C. fun_graph ----
+# B. fun_graph ----
+# Crea graficos segun el estilo indicado
 #--------------------------#
 
 fun_graph <- function(df, type, palette, statistic, path, style){
+  
+  # Tema
+  theme <- list(
+    theme_classic(base_size = text*1.5),
+    theme(axis.title.x = element_text(colour = "black"),
+          axis.title.y = element_text(colour = "black"),
+          axis.text.x = element_text(colour = "black"),
+          axis.text.y = element_text(colour = "black"),
+          legend.position = "bottom",
+          plot.title = element_text(hjust = 0.5),
+          legend.title = element_blank())
+  )
+  
+  # df <- data
+  # type <-  "promedio_anual"
+  # style <- "bar"
+  # statistic <- "mean"
+  # palette <- bar_colors1
+  
+  # Crear etiquetas ejes X y Y 
+  xlab_time <- ifelse(unique(df$id_time) == 1, "Año",
+                      ifelse(unique(df$id_time == 2, "Mes", 
+                                    ifelse(unique(df$time) == 3, "Día", NULL))))
+  xlab_terr <- ifelse(grepl("dpto", unique(df$id_nivel)), "Departamento",
+                      ifelse(grepl("mpio", unique(df$id_nivel)), "Municipio", NULL))
+  ylab <- unique(df$value_label)
+  
+  # Etiquetas de grafica de promedios temporales (anios, meses, dias)
+  options_time <- list(
+    xlab(glue("\n{xlab_time}")),
+    ylab(glue("{ylab}\n"))
+  )
+  
+  # Etiquetas de grafica de promedios territoriales (departamentos, municipios)
+  options_terr <- list(
+    xlab(glue("\n{xlab_terr}")), 
+    ylab(glue("{ylab}\n"))
+  )
   
   print(glue("Graficando {ylab}, estadistica: {statistic}, tipo: {type}"))
   
@@ -182,7 +159,50 @@ fun_graph <- function(df, type, palette, statistic, path, style){
 }
 
 #-------------------------------------------------------#
-# 2. Graficas ----
+# 1. Abrir datos ----
+#-------------------------------------------------------#
+
+# Abrimos datos de ejemplo
+geih <- readxl::read_xlsx("Data/Ejemplo_base_datos/Output/geih_datos_ejemplo.xlsx", sheet = "departamental") %>%
+  mutate(value_label = "Tasa de desempleo (%)")
+
+# geih_gen <- readxl::read_xlsx("Data/Ejemplo_base_datos/Output/geih_datos_ejemplo.xlsx", sheet = "departamental_genero") %>%
+#   mutate(value_label = "Tasa de desempleo (%)")
+
+# Preparar datos
+data <- fun_data(geih)
+# data <- fun_data(geih_gen)
+
+#-------------------------------------------------------#
+# 2. Opciones ----
+#-------------------------------------------------------#
+
+# Tamanos graficas (ancho, alto, texto, resolucion)
+w <- 4.5*2.5
+h <- 3.2*2.5
+text <- 14
+d <- 900
+s <- 1.2
+a_line <- 0.6
+a_dot <- 0.7
+
+# Colores
+# Fuentes: 
+# 1. https://publications.iadb.org/publications/spanish/document/Pulso-social-en-Am%C3%A9rica-Latina-y-el-Caribe-2017-Legado-familiar-%C2%BFrompemos-el-molde-o-repetimos-patrones.pdf
+# 2. https://publications.iadb.org/publications/spanish/document/Pulso-social-de-Am%C3%A9rica-Latina-y-el-Caribe-2016-Realidades-y-perspectivas.pdf
+
+bar_colors1 <- c("#3d5574", "#74ac54", "#b8544c", "#c0ac84", "#c8dccc", "#b0c4e4", "#d4742c")
+lines_colors1 <- c("#3d5574", "#74ac54")
+countries_colors1 <- c("#759cd4", "#c43424", "#ecbc24", "#385676", "#d4742d","#6dad55", "#a4a4a4", "#643d94")
+
+bar_colors2 <- c("#4c9ccc", "#e4546c", "#fcac24", "#0c0c0c", "#b4b4b4")
+lines_colors2 <- c("#4c9ccc", "#e4546c", "#fcac24", "#0c0c0c")
+
+# Cortes de anios (etiquetas eje X)
+years_break <- c(2001, 2005, 2010, 2015, 2019)
+
+#-------------------------------------------------------#
+# 3. Graficas ----
 #-------------------------------------------------------#
 
 # Barras
@@ -197,8 +217,6 @@ fun_graph(df = data, type = "promedio_anual_gen", palette = bar_colors1, statist
 
 
 # Lineas
-years_break <- c(2001, 2005, 2010, 2015, 2019)
-
 fun_graph(df = data, type = "promedio_anual", palette = lines_colors2, statistic = "mean",
           style = "lines", path = "Descriptives/Ejemplo_tasa_desempleo")
 
@@ -207,4 +225,12 @@ fun_graph(df = data, type = "promedio_dpto", palette = countries_colors1, statis
 
 fun_graph(df = data, type = "promedio_anual_gen", palette = bar_colors1, statistic = "mean", 
           style = "lines", path = "Descriptives/Ejemplo_tasa_desempleo")
+
+#-------------------------------------------------------#
+# 4. Mapas ----
+#-------------------------------------------------------#
+
+
+
+
 
