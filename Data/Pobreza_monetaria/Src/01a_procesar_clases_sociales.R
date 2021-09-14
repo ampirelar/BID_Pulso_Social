5db050e6748f2c5007b8d42277f4de77c72ef877
ampirelar <ampirelar@eafit.edu.co>
2021-09-14 11:59:51 -0500

    updates

---
+++ Data/Pobreza_monetaria/Src/01a_procesar_clases_sociales.R
@@ -0,0 +1,104 @@
+#-------------------------------------------------------#
+# Pulso Social BID ----
+# Ultima fecha de modificacion: 14 sept, 2021
+# Procesamiento de datos de Poblacion por clases sociales (pobreza monetaria)
+#-------------------------------------------------------#
+
+#--------------------------#
+# packages ----
+#--------------------------#
+
+rm(list=ls())
+pacman::p_load(tidyverse, glue, readxl)
+# .rs.restartR()
+
+#--------------------------#
+# paths ----
+#--------------------------#
+
+datos_ori <- "Data/Pobreza_monetaria/Input"
+datos <- "Data/Pobreza_monetaria/Output"
+options(scipen = 999)
+
+#-------------------------------------------------------#
+# 1. Procesar datos originales ----
+#-------------------------------------------------------#
+
+# Leer base original
+pob <- read_excel(glue("{datos_ori}/anexo-pobreza-monetaria-caracterizacion-clases-sociales-2020.xls"), sheet = "Clases Sociales Abs. Act.Met")
+
+# Corregimos nombres de columnas
+colnames(pob)[1] <- "variable"
+col_names <- pob[9,] %>% janitor::row_to_names(row_number = 1) 
+col_names <- names(col_names)
+col_names <- col_names[!is.na(col_names)]
+col_names <- c("variable", col_names)
+pob <- pob %>% drop_na(variable) 
+
+# Identificamos clases sociales 
+pob$variable <- gsub("Población según clases sociales - ", "", pob$variable)
+pob$variable <- gsub("23.*", "", pob$variable)
+pob$variable <- trimws(pob$variable)
+
+# Organizar anios 
+pob <- pob %>% filter(!(variable == "Cifras en miles" | variable == "Dominio"))
+names(pob) <- col_names
+pob <- pob %>% 
+  rename_at(vars(contains("20")), funs(paste0("year_", .))) %>% 
+  janitor::clean_names() %>%
+  mutate(across(starts_with("year"), ~ifelse(is.na(.x), variable, .x)))
+
+# Creamos base de clases sociales 
+pob_long <- pob %>%
+  pivot_longer(cols = starts_with("year"), names_to = "year", values_to = "poblacion")
+
+pobres <- pob_long[which(pob_long$variable == "Pobres")[1]:which(pob_long$variable == "Centros poblados y rural disperso")[9], ] %>%
+  mutate(clase = "Pobres") %>% filter(variable != clase)
+vulnerables <- pob_long[which(pob_long$variable == "Vulnerables")[1]:which(pob_long$variable == "Centros poblados y rural disperso")[9*2], ] %>%
+  mutate(clase = "Vulnerables") %>% filter(variable != clase)
+media <- pob_long[which(pob_long$variable == "Clase media")[1]:which(pob_long$variable == "Centros poblados y rural disperso")[9*3], ] %>%
+  mutate(clase = "Clase media") %>% filter(variable != clase)
+alta <- pob_long[which(pob_long$variable == "Clase alta")[1]:which(pob_long$variable == "Centros poblados y rural disperso")[9*4], ] %>%
+  mutate(clase = "Clase alta") %>% filter(variable != clase)
+
+# Pasamos poblacion de miles a personas
+data_clases <- bind_rows(pobres, vulnerables, media, alta) %>%
+  mutate(year = as.numeric(gsub("year_", "", year)), poblacion = 1000*as.numeric(poblacion))
+
+rm(pobres, vulnerables, alta, media, pob, pob_long)
+
+#-------------------------------------------------------#
+# 2. Estadisticas por nivel de desagregacion ----
+#-------------------------------------------------------#
+
+# Total nacional
+clases_col <- data_clases %>% filter(variable == "Total nacional") %>%
+  mutate(id_data = 13, variable = "clase_social", value_label = "Población por clase social", 
+         id_nivel = "nacional", id_time = 1, time = year, clase = gsub(" ", "_", clase),
+         nivel_value = glue("col_{clase}"), value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value)
+
+# Areas metropolitanas
+clases_am <- data_clases %>% 
+  filter(variable != "Total nacional" & variable != "Cabeceras" & variable != "Centros poblados y rural disperso") %>%
+  mutate(id_data = 13, am = gsub(" ", "_", variable), variable = "clase_social", 
+         value_label = "Población por clase social", id_nivel = "area_metropolitana", 
+         id_time = 1, time = year, clase = gsub(" ", "_", clase),
+         nivel_value = glue("{am}_{clase}"), value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value)
+
+# Cabeceras y zonas rurales (centros poblados y rural disperso)
+clases_zona <- data_clases %>% 
+  filter(variable == "Cabeceras" | variable == "Centros poblados y rural disperso") %>%
+  mutate(id_data = 13, zona = gsub(" ", "_", variable), variable = "clase_social", 
+         value_label = "Población por clase social", id_nivel = "zona", 
+         id_time = 1, time = year, clase = gsub(" ", "_", clase),
+         nivel_value = glue("{zona}_{clase}"), value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value)
+
+# Exportar base
+write_csv(clases_col, glue("{datos}/base_clases_sociales_col_2012-2020.csv"))
+write_csv(clases_am, glue("{datos}/base_clases_sociales_am_2012-2020.csv"))
+write_csv(clases_zona, glue("{datos}/base_clases_sociales_zona_2012-2020.csv"))
+
+rm(list = ls(pattern = "clase"))
--- Data/Proyecciones_poblacion/Src/01a_procesar_proyecciones_poblacion.R
+++ Data/Proyecciones_poblacion/Src/01a_procesar_proyecciones_poblacion.R
@@ -25,42 +25,66 @@ options(scipen = 999)
 #-------------------------------------------------------#
 
 # fun_pob: Organiza informacion de proyecciones de poblacion
-fun_pob <- function(data, edad = NULL){
-
+fun_pob <- function(data, edad = NULL, genero = NULL){
+  
   # Limpiamos NA y organizamos nombres
   data <- data %>% drop_na(`...2`) %>% janitor::row_to_names(row_number = 1)
   names(data) <- tolower(names(data))
   
   if(!is.null(edad)){
     data <- data %>% dplyr::rename(cod_mpio = dpmp, year = año, area = `área geográfica`) 
-  } else {
+  } 
+  if(!is.null(genero)){
+    data <- data %>% 
+      dplyr::rename(cod_mpio = dpmp, hombres = `total hombres`, mujeres = `total mujeres`, year = año) %>% 
+      dplyr::select(cod_mpio, year, hombres, mujeres) %>%
+      pivot_longer(cols = c("hombres", "mujeres"), names_to = "genero", values_to = "poblacion")
+  } 
+  
+  if(is.null(edad) & is.null(genero)){
     data <- data %>% dplyr::rename(cod_mpio = dpmp, poblacion = total, year = año) %>% 
       dplyr::select(cod_mpio, year, poblacion)
   }
+  
   return(data)
 }
 
 # fun_edad: Organiza poblacion por edades, municipio y anio
-fun_edad <- function(data){
+fun_edad <- function(data, genero = NULL){
   
   # Reshape wide to long
   data <- data %>% dplyr::filter(area == "Total") %>%
     dplyr::select(cod_mpio, year, starts_with(c("hombres", "mujeres"))) %>%
     pivot_longer(cols = starts_with(c("hombres", "mujeres")), names_to = "edad", values_to = "poblacion")
   
-  # Limpiamos edades
-  data$edad <- gsub("hombres_", "", data$edad) 
-  data$edad <- gsub("mujeres_", "", data$edad) 
-  data$edad <- gsub("y más", "", data$edad)
-  data$edad <- as.numeric(data$edad)
-  
   # Agrupamos poblacion por edades 
-  data <- data %>% 
-    mutate_all(as.numeric) %>%
-    group_by(cod_mpio, year, edad) %>%
-    summarise(poblacion = sum(poblacion)) %>%
-    ungroup()
-  
+  if(!is.null(genero)){
+    
+    # Limpiamos edades y genero
+    data <- data %>% mutate(genero = gsub("_.*", "", edad))
+    data$edad <- gsub("hombres_", "", data$edad) 
+    data$edad <- gsub("mujeres_", "", data$edad) 
+    data$edad <- gsub("y más", "", data$edad)
+    data$edad <- as.numeric(data$edad)
+    
+    data <- data %>%
+      mutate(cod_mpio = as.numeric(cod_mpio), year = as.numeric(year), poblacion = as.numeric(poblacion)) 
+    
+  } else {
+    
+    # Limpiamos edades
+    data$edad <- gsub("hombres_", "", data$edad) 
+    data$edad <- gsub("mujeres_", "", data$edad) 
+    data$edad <- gsub("y más", "", data$edad)
+    data$edad <- as.numeric(data$edad)
+    
+    data <- data %>% 
+      mutate_all(as.numeric) %>%
+      group_by(cod_mpio, year, edad) %>%
+      summarise(poblacion = sum(poblacion)) %>%
+      ungroup()
+  }
+
   return(data)
 }
 
@@ -95,3 +119,89 @@ table(panel_pob$time)
 write_csv(panel_pob, glue("{datos}/base_proyecciones_poblacion_mpio_1985-2035.csv"))
 rm(panel_pob, pob1, pob2)
 
+#--------------------------#
+# B. Por edad ----
+#--------------------------#
+
+# Poblacion 2005-2017
+pob1 <- readxl::read_xlsx(glue("{datos_ori}/anexo-area-sexo-edad-proyecciones-poblacion-Municipal_2005-2017.xlsx")) 
+pob1 <- fun_pob(pob1, edad = 1)
+pob1 <- fun_edad(pob1)
+
+# Poblacion 2018-2026
+pob2 <- readxl::read_xlsx(glue("{datos_ori}/anexo-proyecciones-poblacion-Municipal_2018-2026.xlsx")) 
+pob2 <- fun_pob(pob2, edad = 1)
+pob2 <- fun_edad(pob2)
+
+# Unimos proyecciones anuales y organizamos
+panel_pob <- bind_rows(pob1, pob2) %>% 
+  arrange(cod_mpio, year, edad) %>%
+  mutate(id_data = 17, variable = "poblacion", id_nivel = "mpio_edad", 
+         nivel_value = glue("{cod_mpio}_{edad}"), 
+         id_time = 1, time = year, value_label = "Población", value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value)
+
+table(panel_pob$time)
+colSums(is.na(panel_pob))
+
+# Exportar base
+write_csv(panel_pob, glue("{datos}/base_proyecciones_poblacion_mpio_edad_2005-2026.csv"))
+rm(panel_pob, pob1, pob2)
+
+#--------------------------#
+# C. Genero ----
+#--------------------------#
+
+# Poblacion 1985-2017
+pob1 <- readxl::read_xlsx(glue("{datos_ori}/anexo-sexo-proyecciones-poblacion-Municipal_Sexo_1985-2017.xlsx")) 
+pob1 <- fun_pob(pob1, genero = 1)
+
+# Poblacion 2018-2035
+pob2 <- readxl::read_xlsx(glue("{datos_ori}/anexo-proyecciones-poblacion-Municipal_Sexo_2018-2035.xlsx"))
+pob2 <- fun_pob(pob2, genero = 1)
+
+# Unimos proyecciones anuales y organizamos
+panel_pob <- bind_rows(pob1, pob2) %>% 
+  arrange(cod_mpio, year) %>% 
+  mutate(genero = ifelse(genero == "hombres", "1", "2")) %>%
+  mutate_all(as.numeric) %>%
+  mutate(id_data = 17, variable = "poblacion", id_nivel = "mpio_gen", nivel_value = glue("{cod_mpio}_{genero}"), 
+         id_time = 1, time = year, value_label = "Población", value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value) 
+
+table(panel_pob$time)
+
+# Exportar base
+write_csv(panel_pob, glue("{datos}/base_proyecciones_poblacion_mpio_gen_1985-2035.csv"))
+rm(panel_pob, pob1, pob2)
+
+#--------------------------#
+# D. Edad genero ----
+#--------------------------#
+
+# Poblacion 2005-2017
+pob1 <- readxl::read_xlsx(glue("{datos_ori}/anexo-area-sexo-edad-proyecciones-poblacion-Municipal_2005-2017.xlsx")) 
+pob1 <- fun_pob(pob1, edad = 1)
+pob1 <- fun_edad(pob1, genero = 1)
+
+# Poblacion 2018-2026
+pob2 <- readxl::read_xlsx(glue("{datos_ori}/anexo-proyecciones-poblacion-Municipal_2018-2026.xlsx")) 
+pob2 <- fun_pob(pob2, edad = 1)
+pob2 <- fun_edad(pob2, genero = 1)
+
+# Unimos proyecciones anuales y organizamos
+panel_pob <- bind_rows(pob1, pob2) %>% 
+  arrange(cod_mpio, year, genero, edad) %>%
+  mutate(genero = ifelse(genero == "hombres", "1", "2"),
+         id_data = 17, variable = "poblacion", id_nivel = "mpio_gen_edad", 
+         nivel_value = glue("{cod_mpio}_{genero}_{edad}"), 
+         id_time = 1, time = year, value_label = "Población", value = poblacion) %>%
+  select(id_data, variable, id_nivel, nivel_value, id_time, time, value_label, value)
+
+head(panel_pob)
+table(panel_pob$time)
+colSums(is.na(panel_pob))
+
+# Exportar base
+write_csv(panel_pob, glue("{datos}/base_proyecciones_poblacion_mpio_gen_edad_2005-2026.csv"))
+rm(panel_pob, pob1, pob2)
